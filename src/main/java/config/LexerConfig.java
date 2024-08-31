dd42f3bc2fe62501036facd8032908d25ba6a7f0
lagosmanuel <lagosmanual@gmail.com>
2024-08-31 14:08:04 -0300

    lex: change EOF constant

--- src/main/java/config/LexerConfig.java
+++ src/main/java/config/LexerConfig.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class LexerConfig {
     public static final boolean CONTINUE_ON_ERROR = true;
     public static final boolean ENABLE_OPTIMIZATION = true;
-    public static final char END_OF_FILE = '\uFFFF';
+    public static final char END_OF_FILE = Character.MAX_VALUE;
     public static final char NEWLINE = '\n';
     public static final char CARRY_RETURN = '\r';
     public static final int MAX_INT_LENGTH = 9;
