92f89aa2ba952078fea375a45c93824761eeee43
lagosmanuel <lagosmanual@gmail.com>
2024-09-07 19:40:40 -0300

    token: change the EOF lexeme to the empty string

--- src/main/java/messages/TokenMessages.java
+++ src/main/java/messages/TokenMessages.java
@@ -2,5 +2,5 @@ package main.java.messages;
 
 public class TokenMessages {
     public static final String FORMAT = "(%s,%s,%d)";
-    public static final String EOF = "$";
+    public static final String EOF = "";
 }
