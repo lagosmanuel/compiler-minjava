e31c0a91df71f14c8c7f1053bce4b365bafcae31
lagosmanuel <lagosmanual@gmail.com>
2024-10-06 14:41:28 -0300

    semantic: add test case

---
+++ resources/test-cases/semantic/sinErrores/correct32.java
@@ -0,0 +1,13 @@
+///[SinErrores]
+abstract class B {
+    abstract void m1(int n);
+}
+
+abstract class A extends B {
+    abstract void m1(int n);
+}
+
+class Init{
+    static void main()
+    { }
+}
