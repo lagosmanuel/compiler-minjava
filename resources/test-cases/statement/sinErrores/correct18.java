86b1421c9a31316766b363745493cb7f850239b9
lagosmanuel <lagosmanual@gmail.com>
2024-10-29 18:30:17 -0300

    semantic: fix test case

--- resources/test-cases/statement/sinErrores/correct18.java
+++ resources/test-cases/statement/sinErrores/correct18.java
@@ -4,23 +4,22 @@ class Init{
         if (true) {
             return 0;
         } else {
-            for (int i = 0; i < 10; i+=1) {
-                switch (i) {
-                    case 0: {
-                        return 1;
-                    }
-                    case 1: {
-                        return 2;
-                    }
-                    default: {
-                        if (i == 2) {
-                            return 3;
-                        } else {
-                            while (i < 10) {
+            int i = 0;
+            switch (i) {
+                case 0: {
+                    return 1;
+                }
+                case 1: {
+                    return 2;
+                }
+                default: {
+                    if (i == 2) {
+                        return 3;
+                    } else {
+                        while (i < 10) {
 
-                            }
-                            return 4;
                         }
+                        return 4;
                     }
                 }
             }
