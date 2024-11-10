a295787f0111185681e2150ec5e95f9458cf9374
lagosmanuel <lagosmanual@gmail.com>
2024-11-10 00:29:59 -0300

    codegen: generate chained variable access code

--- src/main/java/semantic/entities/model/statement/chained/ChainedVar.java
+++ src/main/java/semantic/entities/model/statement/chained/ChainedVar.java
@@ -1,14 +1,19 @@
 package main.java.semantic.entities.model.statement.chained;
 
 import main.java.model.Token;
+import main.java.model.TokenType;
 import main.java.semantic.SymbolTable;
 import main.java.semantic.entities.Class;
 import main.java.semantic.entities.Attribute;
 import main.java.semantic.entities.model.Type;
 import main.java.messages.SemanticErrorMessages;
 import main.java.exeptions.SemanticException;
+import main.java.codegen.Instruction;
+import main.java.codegen.Comment;
 
 public class ChainedVar extends Chained {
+    private Attribute attribute;
+
     public ChainedVar(Token identifier, Chained chained) {
         super(identifier, chained);
     }
@@ -27,7 +32,7 @@ public class ChainedVar extends Chained {
     public Type checkType(Type type) throws SemanticException {
         if (type == null || getIdentifier() == null) return null;
         Class myclass = SymbolTable.getClass(type.getName());
-        Attribute attr = myclass != null? myclass.getAttribute(getIdentifier().getLexeme()):null;
+        attribute = myclass != null? myclass.getAttribute(getIdentifier().getLexeme()):null;
 
         if (myclass == null) {
             SymbolTable.throwException(
@@ -38,7 +43,7 @@ public class ChainedVar extends Chained {
                 ),
                 getIdentifier()
             );
-        } else if (attr == null) {
+        } else if (attribute == null) {
             SymbolTable.throwException(
                 String.format(
                     SemanticErrorMessages.ATTRIBUTE_NOT_FOUND,
@@ -47,7 +52,7 @@ public class ChainedVar extends Chained {
                 ),
                 getIdentifier()
             );
-        } else if (myclass != SymbolTable.actualClass && attr.isPrivate()) {
+        } else if (myclass != SymbolTable.actualClass && attribute.isPrivate()) {
             SymbolTable.throwException(
                 String.format(
                     SemanticErrorMessages.ATTRIBUTE_PRIVATE,
@@ -57,15 +62,58 @@ public class ChainedVar extends Chained {
                 getIdentifier()
             );
         } else return getChained() != null?
-            getChained().checkType(attr.getType()):
-            attr.getType();
+            getChained().checkType(attribute.getType()):
+            attribute.getType();
 
         return null;
     }
 
     @Override
     public void generate() {
+        if (!isLeftValue() || getChained() != null) loadAttr();
+        else {
+            opPlusMinus();
+            storeAttr();
+        }
+        if (getChained() != null) getChained().generate();
+    }
 
+    private void opPlusMinus() {
+        if (getAssignOp() == null) return;
+        if (!getAssignOp().getType().equals(TokenType.opAssign)) {
+            SymbolTable.getGenerator().write(Instruction.SWAP.toString());
+            SymbolTable.getGenerator().write(Instruction.LOADSP.toString());
+            SymbolTable.getGenerator().write(Instruction.LOADREF.toString(), "2");
+            loadAttr();
+            SymbolTable.getGenerator().write(Instruction.SWAP.toString());
+        }
+        if (getAssignOp().getType().equals(TokenType.opPlusAssign)) {
+            SymbolTable.getGenerator().write(Instruction.ADD.toString(), Comment.ASSIGN_PLUS);
+        } else if (getAssignOp().getType().equals(TokenType.opMinusAssign)) {
+            SymbolTable.getGenerator().write(Instruction.SUB.toString(), Comment.ASSIGN_MINUS);
+        }
+        if (!getAssignOp().getType().equals(TokenType.opAssign)) {
+            SymbolTable.getGenerator().write(Instruction.SWAP.toString());
+        }
+    }
+
+    private void loadAttr() {
+        SymbolTable.getGenerator().write(
+            Instruction.LOADREF.toString(),
+            String.valueOf(attribute.getOffset()),
+            Comment.ATTRIBUTE_LOAD.formatted(getIdentifier().getLexeme())
+        );
+    }
+
+    private void storeAttr() {
+        SymbolTable.getGenerator().write(
+            Instruction.SWAP.toString()
+        );
+        SymbolTable.getGenerator().write(
+            Instruction.STOREREF.toString(),
+            String.valueOf(attribute.getOffset()),
+            Comment.ATTRIBUTE_STORE.formatted(getIdentifier().getLexeme())
+        );
     }
 
     @Override
