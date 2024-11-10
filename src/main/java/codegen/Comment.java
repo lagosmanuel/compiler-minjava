0da60175c5e75ae5d7277a18dadaafbcd9927ffa
lagosmanuel <lagosmanual@gmail.com>
2024-11-10 01:22:14 -0300

    codegen: implement debugPrint() predefined function

--- src/main/java/codegen/Comment.java
+++ src/main/java/codegen/Comment.java
@@ -35,4 +35,5 @@ public class Comment {
     public static final String VT_ACCESS_METHOD = "; Get method %s from VT";
     public static final String ACCESS_STATIC_METHOD = "; Get static method %s";
     public static final String SWAP_ARGUMENTS = "; Swap method arguments and reference";
+    public static final String IPRINT = "; print integer value from stack";
 }
--- src/main/java/semantic/entities/predefined/BlockPredefined.java
+++ src/main/java/semantic/entities/predefined/BlockPredefined.java
@@ -2,9 +2,12 @@ package main.java.semantic.entities.predefined;
 
 import main.java.model.Token;
 import main.java.model.TokenType;
+import main.java.semantic.SymbolTable;
 import main.java.semantic.entities.model.statement.Block;
 import main.java.semantic.entities.model.statement.Return;
 import main.java.semantic.entities.model.statement.expression.Literal;
+import main.java.codegen.Instruction;
+import main.java.codegen.Comment;
 
 public class BlockPredefined extends Block {
     private final MethodPredefined method;
@@ -23,7 +26,17 @@ public class BlockPredefined extends Block {
     @Override
     public void generate() {
         switch (method) {
-            // TODO
+            case debugPrint -> {
+                SymbolTable.getGenerator().write(
+                    Instruction.LOAD.toString(),
+                    "3",
+                    Comment.VAR_LOAD.formatted("i")
+                );
+                SymbolTable.getGenerator().write(
+                    Instruction.IPRINT.toString(),
+                    Comment.IPRINT
+                );
+            }
         }
     }
 
